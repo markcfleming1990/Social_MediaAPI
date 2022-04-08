8ef7f787112e5065ebaf9408797263a3153c6ff5
Mark <markcfleming1990@gmail.com>
2022-04-08 09:27:37 -0400

    worked on models

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: 'Please enter a user name',
+      trim: true
+    },
+    email: {
+      type: String,
+      required: 'please enter a valid email',
+      unique: true,
+      match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', uswerSchema);
+
+module.exports = User;
